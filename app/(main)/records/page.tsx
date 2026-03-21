738e42018de0c7f2e97acaa668ede1ea41a8e561
Summer <lovetung18@gmail.com>
2026-03-21 21:29:17 +0900

    record - recharts tooltip formatter 타입 에러 수정

--- app/(main)/records/page.tsx
+++ app/(main)/records/page.tsx
@@ -177,7 +177,9 @@ function FinishedBooksTab() {
                     fontSize: 12,
                     fontFamily: 'Pretendard, sans-serif',
                   }}
-                  formatter={(v: number) => [`${v}p`, '페이지']}
+                  formatter={(value: number | string | undefined) => [
+                    value !== undefined ? `${value}p` : '', '페이지'
+                  ]}
                   labelFormatter={(label: string) => label}
                 />
                 <Bar dataKey="pages" radius={[6, 6, 0, 0]} minPointSize={40} label={<CustomBarLabel />}>
